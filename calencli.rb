e67f1ea52267727cb67034809929fef2f4f7214a
Martha Huaman <ms.huamann@gmail.com>
2022-08-12 22:37:22 -0500

    FEAT updating the create_event method

--- calencli.rb
+++ calencli.rb
@@ -1,98 +1,98 @@
 # Data
-id = 0
+$id = 0
 events = [
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-08T00:00:00-05:00",
     :title => "Ruby Basics 1",
     :end_date => "",
     :notes => "Ruby Basics 1 notes",
     :guests => %w[Teddy Codeka],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-08T12:00:00-05:00",
     :title => "English Course",
     :end_date => "2022-08-08T13:30:00-05:00",
     :notes => "English notes",
     :guests => ["Stephanie"],
     :calendar=> "english" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-09T00:00:00-05:00",
     :title => "Ruby Basics 2",
     :end_date => "",
     :notes => "Ruby Basics 2 notes",
     :guests => %w[Andre Codeka],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-09T12:45:00-05:00",
     :title => "Soft Skills - Mindsets",
     :end_date => "2021-08-09T13:30:00-05:00",
     :notes => "Some extra notes",
     :guests => ["Diego"],
     :calendar=> "soft-skills" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-10T00:00:00-05:00",
     :title => "Ruby Methods",
     :end_date => "",
     :notes => "Ruby Methods notes",
     :guests => %w[Diego Andre Teddy Codeka],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-10T12:00:00-05:00",
     :title => "English Course",
     :end_date => "2021-08-10T13:30:00-05:00",
     :notes => "English notes",
     :guests => ["Stephanie"],
     :calendar=> "english" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-11T09:00:00-05:00",
     :title => "Summary Workshop",
     :end_date => "2021-08-11T13:30:00-05:00",
     :notes => "A lot of notes",
     :guests => %w[Diego Teddy Andre Codeka],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-11T09:00:00-05:00",
     :title => "Extended Project",
     :end_date => "",
     :notes => "",
     :guests => [],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-12T09:00:00-05:00",
     :title => "Extended Project",
     :end_date => "",
     :notes => "",
     :guests => [],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-12T12:00:00-05:00", 
     :title => "English for Engineers",
     :end_date => "2022-08-12T13:30:00-05:00",
     :notes => "English notes",
     :guests => ["Stephanie"],
     :calendar=> "english" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-13T10:00:00-05:00",
     :title => "Breakfast with my family",
     :end_date => "2022-08-13T11:00:00-05:00",
     :notes => "",
     :guests => [],
     :calendar=> "default" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-13T15:00:00-05:00",
     :title => "Study",
     :end_date => "2022-08-13T20:00:00-05:00",
     :notes => "",
     :guests => [],
     :calendar=> "default" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-18T09:00:00-05:00",
     :title => "Extended Project",
     :end_date => "",
     :notes => "",
     :guests => [],
     :calendar=> "web-dev" },
-  { :id => (id = id.next),
+  { :id => ($id = $id.next),
     :start_date => "2022-08-19T09:00:00-05:00",
     :title => "Extended Project",
     :end_date => "",
@@ -135,29 +135,42 @@ end
 def create_event(events)
   print "date: "
   new_date = gets.chomp
+  while new_date==""
+    puts "Type a valid date: YYYY-MM-DD"
+    print "date: "
+    new_date = gets.chomp
+  end
   print "title: "
   new_title = gets.chomp
+  while new_title==""
+    puts "Cannot be blank"
+    print "title: "
+    new_title = gets.chomp
+  end
   print "calendar: "
   new_calendar = gets.chomp
   print "start_end: "
   new_start_end = gets.chomp
+  while !new_start_end.include? " "
+    puts "'HH:MM HH:MM'or leave it empty"
+    print "start_end: "
+    new_start_end = gets.chomp
+  end
   start_end=new_start_end.split(" ")
-  start_date= DateTime.parse("#{new_date} #{start_end[0]}")
-  start_date_with_format= start_date.strftime('%FT%T%:z')
-  end_date= DateTime.parse("#{new_date} #{start_end[1]}")
-  end_date_with_format= start_date.strftime('%FT%T%:z')
+  while start_end[0] > start_end[1]
+    puts "Cannot end before start"
+    print "start_end: "
+    new_start_end = gets.chomp
+    start_end=new_start_end.split(" ")
+  end
   print "notes: "
   new_notes = gets.chomp
-  new_event={}
-  new_event[:id]=new_date
-  new_event[:start_date]=start_date_with_format
-  new_event[:title]=new_title
-  new_event[:end_date]=end_date_with_format
-  new_event[:notes]=new_notes
-  new_event[:guests]=""
-  new_event[:calendar]=new_calendar
-  # new_event={:id=>(id=events[:id].next),:start_date=>start_date_with_format,:title=>new_title,:end_date=>end_date_with_format,:notes=>new_notes,:guests=>"",:calendar=>new_calendar}
-  events.push(new_event)
+  print "guests: "
+  new_guests = gets.chomp
+  start_date= DateTime.parse("#{new_date} #{start_end[0]}").strftime('%FT%T%:z')
+  end_date= DateTime.parse("#{new_date} #{start_end[1]}").strftime('%FT%T%:z')
+  guests=new_guests.split(",")
+  events.push({id:($id = $id.next), start_date: start_date, title: new_title, end_date: end_date,notes:new_notes, guests:guests})
 end
 
 def show_info_event(events, event_id)
